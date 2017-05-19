dfe11f383c0570d75991e7fc0ce84763484bee5b
Cyndy Ishida <ishidacy@msu.edu>
2017-05-19 18:03:56 -0400

    got the heap printing!

--- Python/Heap.py
+++ Python/Heap.py
@@ -1,3 +1,5 @@
+from math import ceil, log2
+
 def swap(collection, a, b):
     c = collection[a] 
     collection[a] = collection[b]
@@ -19,18 +21,28 @@ class Heap ( object ):
         return str(self.__mList)
 
     def __str__(self ) :
-        return str(self.__mList) + "\nCount: " + str(self.__mCount)
+        printMe = "Heap contains: {} nodes\n".format(self.__mCount)
+        i = 1
+        for k in range(ceil(log2(self.__mCount)) ):
+            for n in range(2**k) :
+                if i <= self.__mCount:
+                    printMe += " {:^d} ".format(self.__mList[ i])
+                    i += 1
+                else:
+                    break
+            printMe += "\n"
+
+        
+        return printMe 
+
 
     def percolateDown(self):
         ''' accurately reheapifies array after element has been removed '''
-
         swap(self.__mList,1, -1 )
         i = 1 #index of current element to place 
         while 2* i < self.__mCount :
-            print(i)
             if  2 * i +1 > self.__mCount:
                 if self.__mList[2* i] < self.__mList[i] :
-
                     swap(self.__mList, 2*i, i)
                     i *= 2
             else:
@@ -43,18 +55,30 @@ class Heap ( object ):
 
 
 
+
+    def percolateUp(self):
+        '''accurately maintains heap after element has been added '''
+        i = self.__mCount 
+        while i//2 != 0:
+            if self.__mList[i // 2] < self.__mList[i]:
+                swap(self.__mList, i//2, i)
+                i //= 2
+            else:
+                break # if we dont ever swap, that means we are in the correct location
+
+
+
+
     def remove(self):
+        self.__mCount -= 1
         self.percolateDown()
         self.__mList = self.__mList[:-1]
 
     def add(self, element):
         self.__mList.append(element)
-        self.__mCount +=1 
-        if self.__mCount:
-            pass
-            
-                        
-    
+        self.__mCount +=1
+        self.percolateUp()
+
 
 
 
@@ -64,10 +88,10 @@ class Heap ( object ):
 
 def main():
      heap = Heap(["x", 20,10,11,7,2,4,5,3])
+     heap.add(15)
+     heap.add(25)
+     heap.add(12)
+     heap.add(18)
+     heap.add(8)
      print(heap)
-     heap.remove()
-     print(heap)
-     heap.remove()
-     print(heap)
-
 main()
