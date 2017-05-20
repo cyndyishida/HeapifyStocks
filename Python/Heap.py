01afe119ed514dd983828adacd4572e9c789419b
David Ackley <ackleyd1@msu.edu>
2017-05-20 04:55:31 -0400

    pathing

--- Python/Heap.py
+++ Python/Heap.py
@@ -2,50 +2,48 @@ from math import ceil, log2
 import random
 
 def swap(collection, a, b):
-    c = collection[a] 
+    c = collection[a]
     collection[a] = collection[b]
     collection[b] = c
 
-
-
-
-
-class Heap ( object ):
-
-    def __init__(self ,lst = ["x"], greaterThan = True ):
+class Heap(object):
+    def __init__(self, lst = ["x"], greaterThan = True):
         self.__mList = lst
-
         self.__mCount = len(self.__mList) -1
         self.__max = greaterThan
-    
-    def __repr__(self ) :
+
+    def __repr__(self):
         return self.__str__()
 
-
-    def __str__(self ) :
+    def __str__(self):
         printMe = "Heap contains: {} nodes\n".format(self.__mCount)
+        height = (ceil(log2(self.__mCount)))
         i = 1
-        max_len = ( 2 ** (ceil(log2(self.__mCount)) ) * 2 )
-        for k in range(ceil(log2(self.__mCount)) ):
+        max_len = 2 ** height * 2
+        for k in range(height):
             for n in range(2**k) :
                 if i <= self.__mCount:
                     wid = ceil(max_len/ (2 **k))
-                    num = "{0:{1}>2d}".format(self.__mList[ i ], "0")
-                    printMe += "{0:{1}^{width}}".format(num, "-", width = wid )
+                    num = "{0:{1}>2d}".format(self.__mList[i], "0")
+                    printMe += "{:^{width}}".format(num, width = wid)
                     i += 1
                 else:
                     break
-            if k != ceil(log2(self.__mCount)) -1:
-                printMe += "\n\n"
+            if k != height -1:
+                printMe += "\n"
+                for j in range(2**(height - k - 1) - 1):
+                    for h in range(2**k):
+                        path = '/' + (j+1)*2*' ' + '\\'
+                        wid = ceil(max_len/ (2 **k))
+                        printMe += "{:^{width}}".format(path, width = wid)
+                    printMe += '\n'
 
-
-        return printMe 
-
+        return printMe
 
     def percolateDown(self):
         ''' accurately reheapifies array after element has been removed '''
         swap(self.__mList,1, -1 )
-        i = 1 #index of current element to place 
+        i = 1 #index of current element to place
         while 2* i < self.__mCount :
             if  2 * i +1 > self.__mCount:
                 if self.__mList[2* i] < self.__mList[i] :
@@ -59,21 +57,15 @@ class Heap ( object ):
                     swap(self.__mList, 2*i+1, i)
                     i *= (2 + 1 )
 
-
-
-
     def percolateUp(self):
         '''accurately maintains heap after element has been added '''
-        i = self.__mCount 
+        i = self.__mCount
         while i//2 != 0:
             if self.__mList[i // 2] < self.__mList[i]:
                 swap(self.__mList, i//2, i)
                 i //= 2
             else:
                 break # if we dont ever swap, that means we are in the correct location
-
-
-
 
     def remove(self):
         self.__mCount -= 1
@@ -85,18 +77,9 @@ class Heap ( object ):
         self.__mCount +=1
         self.percolateUp()
 
-
-
-
-
-
-
-
 def main():
     heap = Heap()
     for i in range(32):
         heap.add(random.randint(1, 99))
-
-
     print(heap)
 main()
