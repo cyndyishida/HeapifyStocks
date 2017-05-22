a1b0b7b7e3147e858ec3ac727af9dbfe67bf1640
cyndyishida <ishidacy@msu.edu>
2017-05-22 06:50:20 -0400

    merge changes

--- Python/ctci.py
+++ Python/ctci.py
@@ -1,3 +1,7 @@
+def pprint(col):
+    for i in col:
+        print(i)
+
 #problem 10.1 Sorted Merge
 '''
 
@@ -50,7 +54,7 @@ careful brute force
 
 text justification
 longest common subsequence
-'''
+
 def lcs(x,y):
     # row - y len
     # col - x len
@@ -84,3 +88,140 @@ def lcs(x,y):
             
             
     return " ".join(word[::-1])
+
+
+
+'''
+'''
+edit distance
+operations 
+1. insert
+2. delete
+3. replace
+
+def editDistance(a, b):
+    # a = rows = i
+    # b = cols = j
+    eD = [[i for i in range(len(b) + 1 )] ]
+
+
+    #intializing table
+    for k in range(1, len(a) + 1):
+        eD.append([k] + [0 for i in range( len( b )  )])
+    
+    
+
+
+
+    for i in range( 1, len(a ) +1 ):
+        for j in range ( 1 , len(b) + 1):
+            if a[ i- 1 ] == b[j - 1 ]: # if there are the same value just take the diagonal
+                eD[i][j] = eD[i-1][j-1]
+            else:   # take the smallest operation and add 1 because we must change the current 
+                eD[i][j] = min(eD[i-1][j] , eD[i][j-1] , eD[i -1][j-1 ]) + 1
+
+    pprint(eD)
+
+
+
+editDistance([j for j in "cart"], [i for i in "march" ] )
+'''
+
+
+
+def knapsack(ks, k_sum):
+    knap = [[i for i in range(len(ks) + 1) ]]
+    for k in ks:
+        knap.append([k] + [0 for i in range(len(ks))])
+
+
+
+    for i in range( 1,len(ks) + 1 ):
+        for j in range(1, len(ks)+1 ):
+            # ks[i-1] + knap[i][j-1] # left one +  current item same items 
+            # ks[i-1] + knap[i-1][j-1] # diagional items + current
+            #first check if either exced expected sum
+            # pick greater one 
+            # if both exced them pick which ever one is the greater of the 2 
+            if ks[i-1] + knap[i][j-1] > ks[i-1] + knap[i-1][j-1]:
+                if  ks[i-1] + knap[i][j-1] <= k_sum:
+                    knap[i][j]= ks[i-1] + knap[i][j-1]
+                    continue
+                elif ks[i-1] + knap[i-1][j-1] <= k_sum:
+                    knap[i][j] =  ks[i-1] + knap[i-1][j-1]
+                    continue
+            
+            if ks[i-1] + knap[i-1][j-1] <= k_sum:
+                knap[i][j] =  ks[i-1] + knap[i-1][j-1]
+                continue
+            elif ks[i-1] + knap[i][j-1] <= k_sum:
+                knap[i][j]= ks[i-1] + knap[i][j-1]
+                continue
+
+
+            knap[i][j] = max(knap[i-1][j-1],knap[i][j-1],knap[i-1][j] )
+
+    pprint(knap[-1][-1])
+
+
+
+fp = open('test.txt')
+for x in range(int(fp.readline())):
+    n, k_sum = [int(i) for i in fp.readline().split()]
+    ks = [int(i) for i in fp.readline().split()]
+    knap = [[0 for i in range(len(ks) + 1) ] for j in range(len(ks) +1 )]
+
+
+
+    
+    for i in range( 1,len(ks) + 1 ):
+        for j in range(1, len(ks)+1 ):
+            # ks[i-1] + knap[i][j-1] # left one +  current item same items 
+            # ks[i-1] + knap[i-1][j-1] # diagional items + current
+            #first check if either exced expected sum
+            # pick greater one 
+            # if both exced them pick which ever one is the greater of the 2 
+            current_best = max(knap[i-1][j-1], knap[i][j-1],knap[i-1][j] )
+            if ( ks[i-1] + knap[i][j-1] > current_best or  ks[i-1] + knap[i-1][j-1] > current_best ) and  (ks[i-1] + knap[i-1][j-1] <= k_sum or ks[i-1] + knap[i][j-1] <= k_sum ):
+                if ks[i-1] + knap[i][j-1] > ks[i-1] + knap[i-1][j-1]:
+                    if  ks[i-1] + knap[i][j-1] <= k_sum:
+                        knap[i][j]= ks[i-1] + knap[i][j-1]
+                        continue
+                    elif ks[i-1] + knap[i-1][j-1] <= k_sum:
+                        knap[i][j] =  ks[i-1] + knap[i-1][j-1]
+                        continue
+                
+                if ks[i-1] + knap[i-1][j-1] <= k_sum:
+                    knap[i][j] =  ks[i-1] + knap[i-1][j-1]
+                    continue
+                elif ks[i-1] + knap[i][j-1] <= k_sum:
+                    knap[i][j]= ks[i-1] + knap[i][j-1]
+                    continue
+
+            else:
+            
+                knap[i][j] = max(knap[i-1][j-1], knap[i][j-1],knap[i-1][j] )
+
+
+    print("Outcome: ", end=' ')
+    print(knap[-1][-1]  if knap[-1][-1] <= k_sum else 0 )
+    print (" ")
+
+
+    
+    pprint(knap)
+    print(" ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # Heapify Stocks
 
 ## Project Description
-Project designed for students in CSE 331: Data Structures and Algorithms 
-Tree section: Binary Heaps 
+
+>Project designed for students in CSE 331: Data Structures and Algorithms 
+>Tree section: Binary Heaps 
 
 Students will create a Heap Class natively to mimick an STL implementation, then use that class to create 2 instances of heaps to then create a Securites Portfolio where user enters different streams of revenue and decifits. Then randomly requests the median of activities.
 
