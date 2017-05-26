4a9b803f13e3fd7d866b771ca7d3236e81c470f8
Cyndy Ishida <ishidacy@msu.edu>
2017-05-25 23:25:44 -0400

    order

--- C++/Heap.h
+++ C++/Heap.h
@@ -1,20 +1,24 @@
 #include <vector>
-
+#include <cmath>
+#include <iostream>
 #ifndef HEAP_HEAP_H
 #define HEAP_HEAP_H
 
 
+using namespace std;
+
 
 template <typename T> 
-class CHeap {
+class CHeap 
+{
 
 public:
 
-    void push(T);
-    void pop();
+    void push(const T &element );
+    //void pop();
 
     void percolate_up();
-    void percolate_down();
+    //void percolate_down();
 
     void display();
 
@@ -28,34 +32,56 @@ private:
 
 
 template <typename T>
-void CHeap::push(const T &element)
+void CHeap<T>::push(const T &element)
 {
-    mData.emplace(element);
-    ++mCount;
-    percolate_up(); 
+    // mData.emplace(element);
+    // ++mCount;
+    // percolate_up(); 
 
 }
 
 template <typename T>
-void CHeap::percolate_up()
+void CHeap<T>::percolate_up()
 {
-    auto i = mCount;
-    bool swapping = true;
+    // auto i = mCount;
+    // bool swapping = true;
 
-    while (i /2 != 0  && swaping)
-    {
-        if (mList[i] > mList[i/2] )
-        {
-            swap(mList[i/2] , mList[i]);
-            i /=2;
-        }
-        else
-        {
-            swapping = false;
-        }
-    }
+    // while (i /2 != 0  && swaping)
+    // {
+    //     if (mData[i] > mData[i/2] )
+    //     {
+    //         swap(mData[i/2] , mData[i]);
+    //         i /=2;
+    //     }
+    //     else
+    //     {
+    //         swapping = false;
+    //     }
+    // }
 
 
+}
+
+
+template <typename T>
+void CHeap<T>::display()
+{
+    // cout <<"Heap contains: " << mCount << "nodes.\n";
+    // long height = ceil(log2(mCount));
+    // long i = 1;
+    // long max_len = pow(2, height) * 2;
+    // int wid, max_width;
+
+    // for (int k = 0; k <= height; ++k)
+    // {
+    //     max_width = pow(2,k);
+    //     for (int n= 0; n< max_width ; ++n  )
+    //     {
+    //             if (i <= mCount)
+    //                 cout << mList[i] << " ";
+    //     }
+    //     cout << "\n";
+    // }
 }
 
 
---
+++ C++/stocks.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Heap.h"
+
+using namespace std;
+
+int main()
+{
+   
+
+
+
+    return 0;
+}
